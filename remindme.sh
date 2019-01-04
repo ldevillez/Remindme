410317a6d7f2027f0b4ec7b4025650d56d35431f
Devillez Louis <louis.devillez@student.ecp.fr>
2019-01-04 23:04:49 +0100

    old

--- remindme.sh
+++ remindme.sh
@@ -3,7 +3,13 @@ if [ $# -lt 1 ]; then
 	echo "remindme [time] [instruction]"
 else
   if [ $1 = "update" ]; then
-  	DISPLAY=:0.0 python3 $HOME/.config/remind/remindme.py update
+    export DISPLAY=:0
+    export XDG_CURRENT_DESKTOP=i3
+    export DESKTOP_SESSION=i3
+    export XDG_SESSION_DESKTOP=i3
+    export GDMSESSION=i3
+    env > $HOME/tmp.txt
+  	python3 $HOME/.config/remind/remindme.py update
   else
     if [ $# -lt 2 ]; then
     	echo "remindme [time] [instruction]"
